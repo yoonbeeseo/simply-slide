6a6428e946d3aa74526faca47743c4533331fb56
yoonbeeseo <yoon.tec.info@gmail.com>
2025-02-24 13:00:51 +0900

    update

--- src/component/ui/index.ts
+++ src/component/ui/index.ts
@@ -1,6 +1,6 @@
-export { default as TextInput } from "./TextInput"
 export { default as AppForm } from "./AppForm"
 export * as Button from "./Button"
+export { default as TextInput } from "./TextInput"
 export { default as Alert } from "./Alert"
 export { default as ToastComponent } from "./ToastComponent"
 export { default as Loading } from "./Loading"
